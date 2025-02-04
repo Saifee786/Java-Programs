27400e609be9c2d7e0f416c9fb496c4feef7ca4b
Saifee786 <136897433+Saifee786@users.noreply.github.com>
2025-02-04 14:23:41 +0530

    files adding

---
+++ objects.java
@@ -0,0 +1,21 @@
+class objects{
+     static class Animal{
+        public static void dog(){
+          System.out.println("Bow   Bow------!!");
+ 
+        }
+    }
+    class Animal1{
+        public void cat(){
+            System.out.println("Meoww   Meoww----!!");
+        }
+    }
+
+    
+        public static void main(String args[]){
+            Animal obj =new Animal();
+            obj.dog();
+        }
+    }
+
+
